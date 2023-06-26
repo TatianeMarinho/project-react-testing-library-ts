0a6cc504019736c9420f9d5737f0d32dde51fec9
TatianeMarinho <devtatianemarinho@gmail.com>
2023-06-26 14:18:32 -0400

    começando o projeto

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,5 @@
-test('', () => {});
+describe('Testando o componente <App.tsx />', () => {
+  test('Testando se o topo da aplicação contém um conjunto fixo de links de navegação(Home, About, Favorite Pokémon)', () => {
+
+  });
+});
