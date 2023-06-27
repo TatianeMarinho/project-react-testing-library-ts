be750ec5d9bc33d8f93a0e724d53f6a6a6a7187d
TatianeMarinho <devtatianemarinho@gmail.com>
2023-06-26 22:11:51 -0400

    enviando a quarta

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.tsx />', () => {
+  test('A página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<App />, { route: '/digimon' });
+    // buscar elementos:
+    const title = screen.getByRole('heading', { name: /page requested not found/i, level: 2 });
+    // aferir:
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Existe uma imagem com o src https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<App />, { route: '/digimon' });
+    // buscar elementos:
+    const image = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    // aferir:
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
